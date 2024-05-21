16cd6d920860f6dc159a56428197a99d3b38361a
ravana09 <pradeep09sathish@gmail.com>
2024-05-21 16:45:12 +0530

    "Formatted code, added 5000ms delay to setTimeout in useEffect, and removed newline at end of file."

--- src/Components/UseEffectHooks.js
+++ src/Components/UseEffectHooks.js
@@ -1,21 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function UseEffectHooks() {
-    const [count, setCount] = React.useState(0)
-    const [state,setState] = useState(false)
-    useEffect (() => {
-        setTimeout(()=>{
-            setCount(count + 1)
-        })
-    },[state])
-      
+  const [count, setCount] = React.useState(0);
+  const [state, setState] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(count + 1);
+    },5000);
+  });
+
   return (
-    <div>UseEffectHooks
-        <button onClick={()=>{setState(!state)}}>click</button>
-        {count}
-        {state.toString()}
+    <div>
+      UseEffectHooks
+      <button
+        onClick={() => {
+          setState(!state);
+        }}
+      >
+        click
+      </button>
+      {count}
+      {state.toString()}
     </div>
-  )
+  );
 }
 
-export default UseEffectHooks
+export default UseEffectHooks;
