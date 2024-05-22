e4e3a56fa10e71201daf5fb9e63e9dea6d7767bd
ravana09 <pradeep09sathish@gmail.com>
2024-05-22 09:45:11 +0530

    "Refactored ProductApi component to simplify layout and styling of product cards"

--- src/practice/Task/Api/ProductApi.js
+++ src/practice/Task/Api/ProductApi.js
@@ -7,45 +7,34 @@ function ProductApi() {
 
   useEffect(() => {
     fetch("https://dummyjson.com/products")
-     .then((res) => res.json())
+      .then((res) => res.json())
       .then((res) => {
         setData(res.products);
       });
-  },[]);
+  }, []);
   console.log(data);
 
   return (
     <div>
-      <h1>Products</h1>
-      {data.map((i, id) => (
-        <div key={id}>
-            <Row>
-                <Col>
-            <Container>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={i.images[0]} />
-            <Card.Body>
-              <Card.Title>{i.title}</Card.Title>
-              <Card.Text>
-              Brand:  {i.brand}
-              </Card.Text>
-              <Card.Text>
-                {i.description}
-              </Card.Text>
-              <Card.Text>
-               rating: {i.rating}
-              </Card.Text>
-              <Card.Text>
-              Price:{i.price}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          </Container>
-          </Col>
-          </Row>
-          <br/>
-        </div>
-      ))}
+      <Container>
+        <h1>Products</h1>
+        <Row>
+          {data.map((i, id) => (
+            <Col key={id} md={4} className="mb-4">
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={i.images[0]} style={{width:250 ,height:200,margin:20,marginRight:40,padding:10}} />
+                <Card.Body>
+                  <Card.Title>{i.title}</Card.Title>
+                  <Card.Text>Brand: {i.brand}</Card.Text>
+                  <Card.Text>{i.description}</Card.Text>
+                  <Card.Text>Rating: {i.rating}</Card.Text>
+                  <Card.Text>Price: {i.price}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
