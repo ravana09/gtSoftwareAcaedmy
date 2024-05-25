476ea974575660c486c5107e459ca1ff6bee190c
ravana09 <pradeep09sathish@gmail.com>
2024-05-25 14:21:29 +0530

    "Refactor Cart and Shopping components to use useReducer and useContext for state management"

--- src/practice/Task/UseReduce/ShoppingCart/Cart.js
+++ src/practice/Task/UseReduce/ShoppingCart/Cart.js
@@ -1,21 +1,71 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer, useState } from "react";
 import Shopping from "./Shopping";
 
 export let shoppingContext = createContext();
+ 
+function reducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return {
+        count: state.count + 1,
+        text: [...state.text, action.text],
+        totalPrice: state.totalPrice + action.text.price,
+      };
+    case "remove":
+        const updatedText = state.text.filter((_, id) => id !== action.id);
+        console.log(updatedText);
+        const DeletedText = state.text.filter((_, id) => id === action.id);
+        console.log(DeletedText[0].price)
+
+
+      console.log({
+        count: state.count - 1,
+       text: updatedText,
+       totalPrice:state.totalPrice-DeletedText[0].price
+      });
+
+      return {
+        count: state.count - 1,
+      text: updatedText,
+      totalPrice:state.totalPrice-DeletedText[0].price
+      };
+
+    default:
+      break;
+  }
+}
 
 function Cart() {
   let [receive, setReceive] = useState([]);
 
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    text: [],
+    totalPrice: 0,
+  });
+
+  function Delete(id) {
+    dispatch({ type: "remove", id });
+  }
 
   return (
-    <shoppingContext.Provider value={{ setReceive }}>
+    <shoppingContext.Provider value={{ receive, setReceive, dispatch }}>
       <div>Cart</div>
-      
-      <li>{receive.name}</li>
-      <li>{receive.price}</li>
-      
-      
-      
+      <li>
+        {" "}
+        {state.count}
+        <h1> {state.totalPrice}</h1>
+      </li>
+
+      {state.text &&
+        state.text.map((i, id) => (
+          <li key={id}>
+            {i.name}
+            {i.price}
+            <button onClick={() => Delete(id)}>Remove</button>
+          </li>
+        ))}
+
       <Shopping />
     </shoppingContext.Provider>
   );
--- src/practice/Task/UseReduce/ShoppingCart/Shopping.js
+++ src/practice/Task/UseReduce/ShoppingCart/Shopping.js
@@ -2,46 +2,50 @@ import React, { useContext } from "react";
 import { shoppingContext } from "./Cart";
 
 function Shopping() {
-  let { setReceive } = useContext(shoppingContext);
+  let {receive, setReceive, dispatch } = useContext(shoppingContext);
 
   let product = [
     {
-      name: "Product 1",
+      name: "Product 1 ",
       price: 1500,
     },
 
     {
-      name: "Product 2",
+      name: "Product 2 ",
       price: 2000,
     },
 
     {
-      name: "Product 3",
+      name: "Product 3 ",
       price: 2500,
     },
   ];
 
-  console.log(product[0]);
+
 
   function AddProduct(index) {
-    console.log(index);
+ 
+    let data = { ...product[index], id: new Date().getTime() };
+    setReceive( data )
+    console.log(data)
 
-    setReceive(product[index]);
+    dispatch({ type: "add", text:data});
   }
 
   return (
     <div>
       <h1>Products</h1>
       <ul>
-        {product.map((i, index) => (
-          <li key={index}>
-            {index}
+        {product.map((i, id) => (
+          <li key={id}>
+            
             {i.name}
+
             {i.price}
 
             <button
               onClick={() => {
-                AddProduct(index);
+                AddProduct(id);
               }}
             >
               Add
